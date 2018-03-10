dacb88d1d4825cfffddd716203d35003eb03aef9
Wojtek <wojciechtrefon@gmail.com>
2018-03-10 16:49:56 +0100

    Change App to Main

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import './Main.css';
 
-class App extends Component {
+class Main extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="main">
         Init
       </div>
     );
   }
 }
 
-export default App;
+export default Main;
